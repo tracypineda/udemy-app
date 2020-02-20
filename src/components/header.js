f21eac60a9eddcf19797e80eb37a457803b32a7b
Tracy Pineda <tracypineda@gmail.com>
2020-02-20 10:51:34 -0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Header = () => (
+    <header>
+        <Link to="/">Music Database</Link>
+    </header>
+)
+export default Header;
---
+++ src/components/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+import Header from "../components/header";
+
+
+ const Routes = () => (
+     <BrowserRouter>
+        <Header/>
+     
+     
+     
+     
+     
+     </BrowserRouter>
+ )
+
+ export default Routes;
